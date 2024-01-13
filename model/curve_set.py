ef8a93a471a360536ec3d6c5cc6bdf4e309315b1
mr_undefined_spec <*****@users.noreply.github.com>
2024-01-13 19:40:33 +0900

    Combine segment_set into curve_set

--- model/curve_set.py
+++ model/curve_set.py
@@ -1,19 +1,13 @@
 
-from curve import Curve
+from segment import Segment
+from cubic_bezier_curve import CubicBezierCurve
 class CurveSet:
     def __init__(self):
-        self.__data = []
+        self._data = []
     #end
 
-    def append(self, curve):
-        if not type(curve) is Curve:
-            raise TypeError("The argument of the append method must be a Curve")
-        #end if
-        self.__data.append(curve)
-    #end def
-
     def __getitem__(self, i):
-        return self.__data[i]
+        return self._data[i]
     #end def
 
     def __iter__(self):
@@ -21,11 +15,28 @@ class CurveSet:
         return self
     #end def
     def __next__(self):
-        if self._index >= len(self.__data): raise StopIteration
+        if self._index >= len(self._data): raise StopIteration
         self._index += 1
-        return self.__data[self._index-1]
+        return self._data[self._index-1]
     #end def
 
 #end
 
+class CubicBezierCurveSet(CurveSet):
+    def append(self, curve):
+        if not type(curve) is CubicBezierCurve:
+            raise TypeError("The argument of the append method must be a CubicBezierCurve")
+        #end if
+        self._data.append(curve)
+    #end def
+#end
+
+class SegmentSet(CurveSet):
+    def append(self, segment):
+        if not type(segment) is Segment:
+            raise TypeError("The argument of the append method must be a Segment")
+        #end if
+        self._data.append(segment)
+    #end def
+#end
 
