faa094c1c46a70485739bf0ee399d6c1c78cc88e
mr_undefined_spec <*****@users.noreply.github.com>
2024-01-19 09:06:20 +0900

    Add name in Layer

--- model/layer.py
+++ model/layer.py
@@ -1,30 +1,39 @@
 
 from curve_set import CurveSet
 from curve_set import SegmentSet
+class LayerData:
+    def __init__(self, name, path):
+        self.name = name
+        self.path = path
+    #end
+#end
+
 class Layer:
     def __init__(self):
-        self._data = []
+        self.__data = []
     #end
 
     def __getitem__(self, i):
-        return self._data[i]
+        return self.__data[i]
     #end def
 
     def __iter__(self):
-        self._index = 0
+        self.__index = 0
         return self
     #end def
     def __next__(self):
-        if self._index >= len(self._data): raise StopIteration
-        self._index += 1
-        return self._data[self._index-1]
+        if self.__index >= len(self.__data): raise StopIteration
+        self.__index += 1
+        return self.__data[self.__index-1]
     #end def
 
-    def append(self, curve_set):
+    def append(self, layer_name, curve_set):
+        if not type(layer_name) is str:
+            raise TypeError("The 1st argument \"layer_name\" of the append method must be a str")
         if not isinstance(curve_set, CurveSet):
-            raise TypeError("The argument of the append method must be a CurveSet")
+            raise TypeError("The 2nd argument \"curve_set\" of the append method must be a CurveSet")
         #end if
-        self._data.append(curve_set)
+        self.__data.append( LayerData(layer_name, curve_set) )
     #end def
 #end
 
