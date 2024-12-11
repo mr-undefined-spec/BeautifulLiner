18fb51e1a1188d802f45072a38458800b02bb89b
mr_undefined_spec <*****@users.noreply.github.com>
2024-12-11 17:48:44 +0900

    Devide read method to utils/redear

--- model/compose/svg.py
+++ model/compose/svg.py
@@ -18,86 +18,6 @@ class LayerData:
 
 class Svg:
     #
-    # private
-    #
-    def __split_to_xy(self, point_str):
-        s2 = point_str.replace("-", " -")
-        tmp_items = re.split(r'\s+|\+', s2)
-        return list( filter(None, tmp_items) )
-    #end def
-    
-    # IN  nodeValue of d in path of svg as string
-    # OUT CubicBezierCurve
-    def __make_cubic_bezier_curve_set(self, d_str):
-        curve = CubicBezierCurve()
-    
-        point_strs = re.split("[C|L|M|Z]", d_str)
-        point_strs.pop(0)
-        # exception handling for 1st point
-        items = self.__split_to_xy( point_strs[0].strip() )
-        last_point = Point( float(items[0]), float(items[1]) )
-        point_strs.pop(0)
-    
-        for point_str in point_strs:
-            items = self.__split_to_xy( point_str.strip() )
-            if len(items)==2:
-                p3 = Point( float(items[0]), float(items[1]) )
-                x1, y1, x2, y2 = 0.0, 0.0, 0.0, 0.0
-                if (last_point.x < p3.x):
-                    x1 = (last_point.x*2.0 + p3.x*1.0)/3.0
-                    x2 = (last_point.x*1.0 + p3.x*2.0)/3.0
-                else:
-                    x1 = (last_point.x*1.0 + p3.x*2.0)/3.0
-                    x2 = (last_point.x*2.0 + p3.x*1.0)/3.0
-                #end if
-                if (last_point.y < p3.y):
-                    y1 = (last_point.y*2.0 + p3.y*1.0)/3.0
-                    y2 = (last_point.y*1.0 + p3.y*2.0)/3.0
-                else:
-                    y1 = (last_point.y*1.0 + p3.y*2.0)/3.0
-                    y2 = (last_point.y*2.0 + p3.y*1.0)/3.0
-                #end if
-                p1 = Point(x1, y1)
-                p2 = Point(x2, y2)
-            elif len(items)==6:
-                p1 = Point( float(items[0]), float(items[1]) )
-                p2 = Point( float(items[2]), float(items[3]) )
-                p3 = Point( float(items[4]), float(items[5]) )
-            #end if
-            curve.append( CubicBezierCurveControlPoint(last_point, p1, p2, p3) )
-            last_point = p3
-        #end for
-    
-        return curve
-    #end def
-
-    def __make_layer(self, paths):
-        layer = Layer()
-        for path in paths:
-            layer.append(  self.__make_cubic_bezier_curve_set( path.getAttributeNode('d').nodeValue )  )
-            self.__total_path_num += 1
-        #end for
-        return layer
-    #end
-
-    def __get_group_paths_tuple(self):
-        return_tuple = []
-        for group in self.__doc.getElementsByTagName("g"):
-            the_tuple = []
-            the_tuple.append(group)
-
-            the_paths = []
-            for path in group.getElementsByTagName("path"):
-                the_paths.append(path)
-            #end for
-            the_tuple.append(the_paths)
-
-            return_tuple.append(the_tuple)
-        #end for
-        return tuple(return_tuple)
-    #end
-
-    #
     # public
     #
     def __init__(self, global_calc_step, mode, progress_bar=None, log_text=None):
@@ -114,24 +34,19 @@ class Svg:
 
         self.__progress_bar = progress_bar
         self.__log_text     = log_text
+
+        self.__doc      = None
+        self.__view_box = None
     #end
 
-    def read(self, file_name):
-        if not type(file_name) is str:
-            raise ValueError("file_name must be str")
-        self.__doc = minidom.parse(file_name)
-
-        for group_paths_set in self.__get_group_paths_tuple():
-            group = group_paths_set[0]
-            paths = group_paths_set[1]
-            layer_name = group.getAttributeNode('id').nodeValue
-
-            self.append(layer_name, self.__make_layer(paths) )
-        #end
-        root = self.__doc.getElementsByTagName("svg")
-        self.__view_box = root[0].attributes["viewBox"].value
+    def set_doc(self, val):
+        self.__doc = val
     #end
 
+    @property
+    def doc(self):
+        return self.__doc
+    #end
 
     def set_view_box(self, val):
         self.__view_box = val
@@ -142,10 +57,6 @@ class Svg:
         return ( float(arr[0]), float(arr[1]), float(arr[2]), float(arr[3]) )
     #end
 
-    @property
-    def doc(self):
-        return self.__doc
-    #end
 
     def append(self, layer_name, layer):
         if not type(layer_name) is str:
--- test/utils/test_reader.py
+++ test/utils/test_reader.py
@@ -1,13 +1,16 @@
-
 import os
 import sys
 
 import pprint
 
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../model'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../model/primitive'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../model/curve'))
-sys.path.append(os.path.join(os.path.dirname(__file__), '../../../model/compose'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/primitive'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/curve'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/compose'))
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../utils'))
+
+import reader
 
 from point import Point
 from cubic_bezier_curve import CubicBezierCurve
@@ -19,22 +22,13 @@ from svg import Svg
 import unittest
 
 class TestSvgData(unittest.TestCase):
-    def test_write(self):
-        svg_from_file = Svg(0, "TEST")
-        # just read
-        svg_from_file.read("data/test.svg")
+    def test_create_svg_from_file(self):
+        svg_from_file = reader.create_svg_from_file("data/test.svg")
 
-        return # FIXME skip
-
-        # just write
-        svg_from_file.write("data/write_test.svg")
-
-        reread_svg = Svg(0, "TEST")
-        reread_svg.read("data/write_test.svg")
         xs = []
         ys = []
 
-        for layer in reread_svg:
+        for layer in svg_from_file:
             for curve_set in layer.path_data:
                 for curve in curve_set:
                     for ctrl_p in curve:
@@ -48,8 +42,8 @@ class TestSvgData(unittest.TestCase):
         the_answer_x = [402.67, 399.823, 393.001, 392.271, 392.271, 386.945, 389.765, 404.953, 404.953, 408.446, 420.484, 433.613, 404.732, 404.732, 406.168, 415.213, 415.213, 420.015, 423.805, 429.187, 429.187, 429.783, 430.352, 430.352]
         the_answer_y = [127.291, 128.714, 175.251, 181.822, 181.822, 229.751, 292.499, 338.063, 338.063, 348.544, 380.166, 380.166, 288.481, 308.04, 339.391, 357.48, 357.48, 367.085, 376.613, 386.302, 386.302, 387.375, 389.796, 389.796]
 
-        #self.assertEqual(xs, the_answer_x)
-        #self.assertEqual(ys, the_answer_y)
+        self.assertEqual(xs, the_answer_x)
+        self.assertEqual(ys, the_answer_y)
     #end
 #end
 
