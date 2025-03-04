b314a7ed25b34094e27ed0391fc866c8d1519a16
mr_undefined_spec <*****@users.noreply.github.com>
2025-03-04 18:33:24 +0900

    Add pickup_intersection_candidates_handler

---
+++ handler/pickup_intersection_candidates_handler.py
@@ -0,0 +1,31 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../model/primitive'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../model/curve'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../model/layer'))
+
+from point import Point
+from cubic_bezier_curve_control_point import CubicBezierCurveControlPoint
+from linear_approximate_curve_control_point import LinearApproximateCurveControlPoint
+from linear_approximate_curve import LinearApproximateCurve
+
+from layer import Layer
+from layer_set import LayerSet
+
+from basic_handler import BasicHandler
+
+import numpy as np
+
+class PickupIntersectionCandidatesHandler(BasicHandler):
+    @classmethod
+    def pickup_intersection_candidates(cls, target_curve, other_curves):
+        candidates = []
+        for other_curve in other_curves:
+            if target_curve.rect.test_collision(other_curve.rect):
+                candidates.append(other_curve)
+            #end
+        #end
+        return candidates
+    #end
+#end
---
+++ test/handler/test_pickup_intersection_candidates_handler.py
@@ -0,0 +1,56 @@
+
+import os
+import sys
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/primitive'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/curve'))
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/layer'))
+
+from point import Point
+from cubic_bezier_curve_control_point import CubicBezierCurveControlPoint
+from linear_approximate_curve_control_point import LinearApproximateCurveControlPoint
+from cubic_bezier_curve import CubicBezierCurve
+from linear_approximate_curve import LinearApproximateCurve
+
+from layer_set import LayerSet
+
+
+import unittest
+
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../handler'))
+from pickup_intersection_candidates_handler import PickupIntersectionCandidatesHandler
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../helper'))
+import model_mocks
+import handler_mocks
+from handler_mocks import ArcDirection
+
+from unittest.mock import MagicMock
+
+import numpy as np
+import math
+
+class TestDeleteEdgeHandler(unittest.TestCase):
+    def test_delete_edge(self):
+        curve_1 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0,    0.0,   0.0,   0.0,  90.0, 100, ArcDirection.CLOCKWISE)
+        curve_2 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0,  100.0,   0.0,  90.0, 180.0, 100, ArcDirection.CLOCKWISE)
+        curve_3 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0,    0.0, 200.0, 180.0, 270.0, 100, ArcDirection.COUNTER_CLOCKWISE)
+        curve_4 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0, -200.0, 200.0,   0.0,  90.0, 100, ArcDirection.CLOCKWISE)
+
+        pickup_intersection_candidates_handler = PickupIntersectionCandidatesHandler()
+        candidates = pickup_intersection_candidates_handler.pickup_intersection_candidates(curve_1, [curve_2, curve_3, curve_4])
+
+        self.assertEqual(candidates[0], curve_2)
+
+    #end
+
+
+#end
+
+if __name__ == '__main__':
+    unittest.main()
+#end
+
