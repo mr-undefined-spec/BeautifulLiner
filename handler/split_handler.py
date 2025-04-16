cc222af17ced225e472121ff3d3a7cc623bb748b
mr_undefined_spec <*****@users.noreply.github.com>
2025-04-16 17:37:03 +0900

    Combine curve_orientation_handler into split_handler

--- handler/split_handler.py
+++ handler/split_handler.py
@@ -1,4 +1,5 @@
-#import numpy as np
+import math
+import numpy as np
 
 #import os
 #import sys
@@ -9,7 +10,115 @@ from basic_handler import BasicHandler
 class SplitHandler(BasicHandler):
 
     @staticmethod
-    def process(curve_orientations, index_offset):
+    def get_the_distance_of_perpendicular_intersection_point_from_point(start, point, end):
+        # Calculate the differences
+        dx = end.x - start.x
+        dy = end.y - start.y
+        
+        # If the segment is a point, return the point itself
+        if dx == 0 and dy == 0:
+            return Point(start.x, start.y)
+        #end
+        
+        # Calculate the parameter t for the projection
+        t = ((point.x - start.x) * dx + (point.y - start.y) * dy) / (dx * dx + dy * dy)
+        
+        # Compute the intersection point
+        intersection_x = start.x + t * dx
+        intersection_y = start.y + t * dy
+
+        dist = ( point.x - intersection_x )*( point.x - intersection_x ) + ( point.y - intersection_y )*( point.y - intersection_y )
+        dist = math.sqrt(dist)
+        
+        return dist
+    #end
+
+    @staticmethod
+    def __are_all_points_close_to_line(points, skip_size, eps_dist):
+        are_all_points_close_to_line = True
+
+        for i in range(skip_size, len(points)-skip_size):
+            dist = SplitHandler.get_the_distance_of_perpendicular_intersection_point_from_point(points[0], points[i], points[-1])
+            if dist > eps_dist:
+                are_all_points_close_to_line = False
+            #end
+        #end
+
+        return are_all_points_close_to_line
+    #end
+
+    @staticmethod
+    def __create_curve_orientations_with_3points_relative_position(points, skip_size):
+        curve_orientations = []
+
+
+        # 点列を3点ずつ取り出す
+        for i in range(skip_size, len(points)-skip_size):
+            p1, p2, p3 = points[0], points[i], points[-1]
+            
+            # ベクトルを計算
+            v1 = [p2.x - p1.x, p2.y - p1.y]
+            #v1 = [p1.x - p2.x, p1.y - p2.y]
+            v2 = [p3.x - p2.x, p3.y - p2.y]
+            length_v1 = np.linalg.norm(v1)
+            length_v2 = np.linalg.norm(v2)
+            
+            # 外積の大きさ（曲率の分子）を計算
+            cross_product = np.cross(v1, v2) / length_v1 / length_v2
+            #cross_product = (v1[0]*v2[1] - v1[1]*v2[0]) / length_v1 / length_v2
+
+            theta = np.arcsin(cross_product)
+            theta_degree = np.degrees(theta)
+
+            """
+            if theta_degree > 15:
+                print(dist)
+                print(theta_degree)
+                print(p1)
+                print(p2)
+                print(p3)
+                print(v1)
+                print(v2)
+                print(length_v1)
+                print(length_v2)
+                print(cross_product)
+                print("")
+            """
+            if( cross_product > 0.1 ):
+                are_all_cross_products_less_than = False
+            #end
+
+
+            if cross_product > 0:
+                curve_orientations.append(1.0)
+            else:
+                curve_orientations.append(-1.0)
+            #end
+
+            #curve_orientations.append(curvature)
+        #end
+
+        first_element = curve_orientations[0]
+        last_element = curve_orientations[-1]
+        for i in range(skip_size):
+            curve_orientations.insert(0, first_element)
+            curve_orientations.append(last_element)
+        #end
+        return np.array(curve_orientations)
+
+        # old functions not used now
+        """
+        are_all_cross_products_less_than = True
+        if are_all_cross_products_less_than:
+            size = len(curve_orientations)
+            return np.ones(size)
+        else:
+        """
+
+    #end
+
+    @staticmethod
+    def __create_split_curve_ranges(curve_orientations, index_offset):
         split_curve_ranges = []
         current_value = curve_orientations[0]
 
@@ -26,4 +135,34 @@ class SplitHandler(BasicHandler):
         split_curve_ranges.append((pre_index + index_offset, len(curve_orientations) + index_offset))  # 最後の区間を追加
         return split_curve_ranges
     #end
+
+    @staticmethod
+    def __create_curve_orientations(curve):
+        points = curve.get_points()
+        #print(points)
+
+        skip_ratio = 0.1
+        skip_size = int( len(points)*skip_ratio ) 
+
+        if SplitHandler.__are_all_points_close_to_line(points, skip_size, 1.0):
+            return np.ones(size)
+        #end
+
+        curve_orientations = SplitHandler.__create_curve_orientations_with_3points_relative_position(points, skip_size)
+
+        once_split_curve_ranges = SplitHandler.__create_split_curve_ranges(curve_orientations, 0)
+
+
+        return curve_orientations
+    #end
+
+    @staticmethod
+    def process(curve, index_offset):
+        curve_orientations = SplitHandler.__create_curve_orientations(curve)
+        split_curve_ranges = SplitHandler.__create_split_curve_ranges(curve_orientations, index_offset)
+
+        return split_curve_ranges
+    #end
+
+
 #end
--- test/handler/test_split_handler.py
+++ test/handler/test_split_handler.py
@@ -3,6 +3,12 @@ import unittest
 import os
 import sys
 
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/primitive'))
+from point import Point
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '../../model/curve'))
+from linear_approximate_curve import LinearApproximateCurve
+
 sys.path.append(os.path.join(os.path.dirname(__file__), '../../handler'))
 from split_handler import SplitHandler
 
@@ -13,13 +19,33 @@ from handler_mocks import ArcDirection
 
 
 class TestSplitHandler(unittest.TestCase):
+    def setUp(self):
+
+        curve_1 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0,    0.0,   0.0,   0.0,  90.0, 100, ArcDirection.CLOCKWISE)
+        curve_2 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0,    0.0, 200.0, 180.0, 270.0, 100, ArcDirection.COUNTER_CLOCKWISE)
+        curve_3 = handler_mocks.create_mock_linear_approximate_curve_of_arc(100.0, -200.0, 200.0,   0.0,  90.0, 100, ArcDirection.CLOCKWISE)
+
+        the_curve = LinearApproximateCurve()
+        for ctrl_p_1 in curve_1:
+            the_curve.append(ctrl_p_1)
+        #end
+        for ctrl_p_2 in curve_2:
+            the_curve.append(ctrl_p_2)
+        #end
+        for ctrl_p_3 in curve_3:
+            the_curve.append(ctrl_p_3)
+        #end
+
+        self.curve = the_curve
+    #end
+
 
     def test_split(self):
-        curve_orientations = [1, 1, 1, -1, -1, -1, 1, 1, 1]
+        #curve_orientations = [1, 1, 1, -1, -1, -1, 1, 1, 1]
 
-        split_curve_ranges = SplitHandler.process(curve_orientations, 0)
+        split_curve_ranges = SplitHandler.process(self.curve, 0)
 
-        self.assertEqual(split_curve_ranges, [(0, 3), (3, 6), (6, 9)])
+        self.assertEqual(split_curve_ranges, [(0, 100), (100, 201), (201, 301)])
     #end
 #end
 
